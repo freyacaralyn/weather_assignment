8934085a7a407ebc7e7e24f12985b38129d4274b
Freya <freyacaralyn@gmail.com>
2024-01-16 12:26:21 +0100

    Part 2: yearly & relative monthly

--- Script.py
+++ Script.py
@@ -1,8 +1,6 @@
-# Part 1
-# 1: find station code for Seattle
+# Part 2
 ## Station code for Seattle: GHCND:US1WAKG0038
 
-# 2: select all the measurements belonging to it
 ## reading the json file
 import json
 with open("weather_assignment/precipitation.json") as file:
@@ -14,9 +12,9 @@ for data in precipitation_data:
     if data["station"] == "GHCND:US1WAKG0038":
         seattle_data.append(data)
 
-#3: calculate the total_monthly_precipitation
-## creating a dictionary to store the sum of precipitation per month
+## creating a dictionary to store the sum of precipitation per month and total_yearly_precipitation
 seattle_monthly_precipitation_dict = {}
+total_yearly_precipitation = 0
 for data in seattle_data:
     date = data["date"]
     month = (date.split("-"))[1]
@@ -24,19 +22,27 @@ for data in seattle_data:
         seattle_monthly_precipitation_dict[month] = data["value"]
     else:
         seattle_monthly_precipitation_dict[month] += data["value"]
-
+    total_yearly_precipitation += data["value"] 
+    
 ## transforming the dictionary into a list
 seattle_monthly_precipitation = []
 for data in seattle_monthly_precipitation_dict:
     seattle_monthly_precipitation.append(seattle_monthly_precipitation_dict[data])
 
-#4: saving the results into json
+## calculating relative_monthly_precipitation
+relative_monthly_precipitation = []
+for data in seattle_monthly_precipitation:
+    calculation = round(data/total_yearly_precipitation, 4)
+    relative_monthly_precipitation.append(calculation)
+
 ## creating the output data
 output = {
     "Seattle":{
             "station": "GHCND:US1WAKG0038",
             "state": "WA",
-            "total_monthly_precipitation": seattle_monthly_precipitation
+            "total_monthly_precipitation": seattle_monthly_precipitation,
+            "total_yearly_precipitation": total_yearly_precipitation,
+            "relative_monthly_precipitation": relative_monthly_precipitation
         }    
 }    
 
